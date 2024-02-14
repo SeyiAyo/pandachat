5d38214100923393e5aaffab9110cf95ff4dfb98
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-02-14 03:04:04 +0100

    build.sh error resolved

--- manage.py
+++ manage.py
@@ -9,12 +9,14 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandachat.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
+    except ImportError:
+        import sys
+        sys.stderr.write(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?\n"
+        )
+        raise
 
     execute_from_command_line(sys.argv)
 
