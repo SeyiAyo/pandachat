213da49b436dcc3f755a2e389266af442b5f0b56
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-02-14 02:43:41 +0100

    cellected admin static files

--- pandachat/urls.py
+++ pandachat/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('rooms/', include('room.urls')),
 ]
+
+urlpatterns += settings.STATIC_ROOT(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
